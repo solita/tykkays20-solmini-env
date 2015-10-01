5a6d059ceeb09c87d37ab65b1808e9a6bc38430a
Petri Sirkkala <sirpete@iki.fi>
2015-10-01 16:40:14 +0300

    Comments

--- build-env.sh
+++ build-env.sh
@@ -5,8 +5,10 @@ service docker start
 docker run -d -p 80:80 \
   -v /var/run/docker.sock:/tmp/docker.sock:ro \
   jwilder/nginx-proxy
+# Generate html content
 mkdir -p /tykkays20/html && \
   echo '<html><body>Hello, world!</body></html>' >/tykkays20/html/index.html
+
 docker run --name tykkays20-nginx \
   -e VIRTUAL_HOST=tmp2-tykkays20 \
   -v /tykkays20/html:/usr/share/nginx/html:ro -d nginx
