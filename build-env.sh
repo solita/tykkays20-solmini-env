8be77759fd2d9cb4b1a50e6637cc94c78a1553c8
Petri Sirkkala <sirpete@iki.fi>
2015-10-02 20:49:53 +0300

    Nimetty edustaproxy

--- build-env.sh
+++ build-env.sh
@@ -5,7 +5,7 @@ service docker start
 
 # Front reverse proxy with virtual host autoconfiguration
 # *** Privileged docker container ***
-docker run -d -p 80:80 \
+docker run -d --name nginx-proxy -p 80:80 -p 443:443 \
   -v /var/run/docker.sock:/tmp/docker.sock:ro \
   jwilder/nginx-proxy
 
@@ -21,7 +21,9 @@ docker run --name solmini \
 # Jenkins master
 docker run -d --name jenkins-master \
   -e VIRTUAL_HOST=jenkins.tykkays20.solita.fi \
+  -e VIRTUAL_HOST=jenkins.solmini.tykkays20.solita.fi \
   -e VIRTUAL_PORT=8080 \
   -e "JAVA_OPTS=-Duser.timezone=EET -Dfile.encoding=UTF8" \
   -p 8080:8080 -p 50000:50000 \
   sirkkalap/jenkins-swarm-w-git-manual:latest
+
